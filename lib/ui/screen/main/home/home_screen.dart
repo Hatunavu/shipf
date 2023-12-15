f0ef7d9e3803c1de43afe88b845e40b864eed92f
namvt@gm-group.vn <namvt@gm-group.vn>
2023-12-15 14:27:45 +0700

    create bill screen

--- lib/ui/screen/main/home/home_screen.dart
+++ lib/ui/screen/main/home/home_screen.dart
@@ -45,6 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: primaryColor,
         elevation: 0,

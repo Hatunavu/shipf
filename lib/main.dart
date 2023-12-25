86626be7a898f3fb2d42a0786f74e0ec52ef1ed3
hatunavu <vutruongnam0@gmail.com>
2023-12-26 00:04:45 +0700

    config noti on ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shipf/app.dart';
@@ -6,6 +7,13 @@ import 'package:shipf/ui/services/ThemePreferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // FirebaseMessaging.instance.requestPermission();
+  // await Future.delayed(Duration(seconds: 1));
+  // FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  // await Future.delayed(Duration(seconds: 1));
+  // String? deviceToken = await firebaseMessaging.getToken();
+  // log('token: $deviceToken');
 
   configureDependencies();
 

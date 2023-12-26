fc656e5097f569374a8dd17807507928744854a3
namvt@gm-group.vn <namvt@gm-group.vn>
2023-12-26 09:50:45 +0700

    config firebase on android

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shipf/app.dart';
+import 'package:shipf/data/remote/firebase/firebase_api.dart';
 import 'package:shipf/injection.dart';
 import 'package:shipf/ui/services/ThemePreferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //firebase
+  await Firebase.initializeApp();
+  await FirebaseApi().initNotifications();
 
   configureDependencies();
 
